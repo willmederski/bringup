b0626f2865983d85f24a3ce8f1981b90fd9118b0
Patricia Silva <pscorreo@gmail.com>
2013-07-25 17:12:38 -0500

    Add school data to schools table

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,5 +1,5 @@
 class School < ActiveRecord::Base
-  attr_accessible :address, :charter_type, :enrollment_as_of_oct_2012, :grade_range, :instruction_type, :school_, 
-  :school_city, :school_email, :school_fax, :school_name, :school_number, :school_phone, 
+  attr_accessible :charter_type, :enrollment_as_of_oct_2012, :grade_range, :instruction_type, :school_email_address, 
+  :school_city, :school_fax, :school_name, :school_number, :school_phone, 
   :school_principal, :school_site_city, :school_site_state, :school_site_street_address, :school_site_zip, :school_state, :school_status, :school_status_date, :school_street_address, :school_web_page_address, :school_zip, :update_date
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+require 'csv'    
+
+CSV.foreach("#{Rails.root}/public/seed_data/schools.csv", :headers => true) do |row|
+  School.create!(row.to_hash)
+end
+
+
+#File.open("#{Rails.root}/public/seed_data/schools.csv") do |schools|
+ # schools.read.each_line do |school|
+   # "row names"=school_number,school_name, instruction_type, charter_type,school_street_address,school_city,school_state,	school_zip,school_site_address,school_site_city,school_site_state,school_site_zip,school_phone,school_fax,school_email_address,school_web_page_address,school_principal,grade_range,school_enrollment	_as_of_oct_2012,school_status,school_status_update,update_date
+  #  school.chomp.split(",")
+    
+    # to create each record in the database
+#     School.create!(
+#     	:school_number => school_number, 
+#     	:school_name => school_name, 
+#     	:instruction_type =>  instruction_type,
+#     	:charter_type => charter_type,
+#     	:school_street_address => school_street_address,
+#     	:school_city => school_city,
+#     	:school_state => school_state,
+#     	:school_site_address => school_site_address,
+#     	:school_site_city => school_site_city,
+#     	:school_site_state => school_site_state,
+#     	:school_site_zip => school_site_zip,
+#     	:school_phone => school_phone,
+#     	:school_fax => school_fax,
+#     	:school_email_address => school_email_address,
+#     	:school_web_page_address => school_web_page_address,
+#     	:school_principal => school_principal,
+#     	:grade_range => grade_range,
+#     	:school_enrollment_as_of_oct_2012 => school_enrollment_as_of_oct_2012,
+#     	:school_status =>	school_status,
+#     	:school_status_update	=> school_status_update,
+#     	:update_date => update_date,
+#     )             
+#   end
+# end
+
+#"row names" = school_number	school_name	instruction_type	charter_type	school_street_address	school_city	school_state	
+#school_zip	school_site_address	school_site_city	school_site_state	school_site_zip	school_phone	school_fax	
+#school_email_address	school_web_page_address	school_principal	grade_range	school_enrollment_as_of_oct_2012	school_status	school_status_update	update_date
