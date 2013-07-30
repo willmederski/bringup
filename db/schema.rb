8dd7871eaf860a4c2441b384f13bebc892de5a8e
Patricia Silva <pscorreo@gmail.com>
2013-07-30 15:38:22 -0500

    add status column to replies table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130730192457) do
+ActiveRecord::Schema.define(:version => 20130730203720) do
 
   create_table "courses", :force => true do |t|
     t.string   "twilio_id"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(:version => 20130730192457) do
     t.string   "uri"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+    t.string   "status"
   end
 
   create_table "schools", :force => true do |t|
