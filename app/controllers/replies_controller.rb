521923a98e7a92c0e077cac8858cb2ce4e404816
Patricia Silva <pscorreo@gmail.com>
2013-08-02 16:14:20 -0500

    additional testing

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -27,63 +27,28 @@ class RepliesController < ApplicationController
 
 
 
-def receive
+  def receive
+    @sms_state = session[:sms_state]
+    if @sms_state.nil?
+      @reply = Reply.new
+      @reply.message_id = params["SmsSid"]
+      @reply.account_sid=params["AccountSid"]
+      @reply.from=params["From"]
+      @reply.body=params["Body"]
+      @reply.status=params["SmsStatus"]
+      @reply.api_version=params["ApiVersion"]
+      get_first_nm(@reply.from)
+      @sms_state = 'welcome'
+    elsif @sms_state == 'welcome'
+      @reply.first_nm=params["Body"]
+      get_last_nm(@reply.from)
+      @sms_state = 'first_nm'   
+    elsif @sms_state == 'first_nm'
+      @reply.last_nm=params["Body"]
+      @sms_state = 'complete'
+    end
 
-  @sms_state = session[:sms_state]
-  if @sms_state.nil?
-    @reply = Reply.new
-    @reply.message_id = params["SmsSid"]
-    @reply.account_sid=params["AccountSid"]
-    @reply.from=params["From"]
-    @reply.body=params["Body"]
-    @reply.status=params["SmsStatus"]
-    @reply.api_version=params["ApiVersion"]
-    
-
-  get_first_nm(@reply.from)
-  @sms_state = 'welcome'
-  elsif @sms_state == 'welcome'
-    @reply.first_nm=params["Body"]
-    get_last_nm(@reply.from)
-    @sms_state = 'first_nm'   
-  elsif @sms_state == 'first_nm'
-    @reply.last_nm=params["Body"]
-    @sms_state = 'complete'
-  end
-end
-
-def get_first_nm(sendto)
-  account_sid = ENV['TWILIO_ACCOUNT_SID']
-  auth_token = ENV['TWILIO_TOKEN']
-  client = Twilio::REST::Client.new account_sid, auth_token
-  from = "+15128618455" # Your Twilio number
-  to = sendto
-
-
-  client.account.sms.messages.create(
-    :from => from,
-    :to => to,
-    :body => "Welcome to BringUp!  Let's begin.  What is your first name?"
-    ) 
-end
-
-def get_last_nm(sendto)
-  account_sid = ENV['TWILIO_ACCOUNT_SID']
-  auth_token = ENV['TWILIO_TOKEN']
-  client = Twilio::REST::Client.new account_sid, auth_token
-  from = "+15128618455" # Your Twilio number
-  to = sendto
-
-
-  client.account.sms.messages.create(
-    :from => from,
-    :to => to,
-    :body => "What is your last name?"
-    ) 
-  puts "Sent message to #{value}"
-end
-
-@respond_to do |format|
+    @respond_to do |format|
       if @reply.save
         #format.html { redirect_to @reply, notice: 'Reply was successfully created.' }
         format.xml{ render xml: @reply, status: :created, location: @reply }
@@ -92,6 +57,40 @@ end
         format.xml { render xml: @reply.errors, status: :unprocessable_entity }
       end
     end
+  end
+
+  def get_first_nm(sendto)
+    account_sid = ENV['TWILIO_ACCOUNT_SID']
+    auth_token = ENV['TWILIO_TOKEN']
+    client = Twilio::REST::Client.new account_sid, auth_token
+    from = "+15128618455" # Your Twilio number
+    to = sendto
+
+
+    client.account.sms.messages.create(
+      :from => from,
+      :to => to,
+      :body => "Welcome to BringUp!  Let's begin.  What is your first name?"
+    ) 
+  end
+
+  def get_last_nm(sendto)
+    account_sid = ENV['TWILIO_ACCOUNT_SID']
+    auth_token = ENV['TWILIO_TOKEN']
+    client = Twilio::REST::Client.new account_sid, auth_token
+    from = "+15128618455" # Your Twilio number
+    to = sendto
+
+
+    client.account.sms.messages.create(
+      :from => from,
+      :to => to,
+      :body => "What is your last name?"
+    ) 
+    puts "Sent message to #{value}"
+  end
+
+
 
 end
 
