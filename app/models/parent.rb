71c3dc5fddc73626e6183b1cf13a31a9b792331b
Patricia Silva <pscorreo@gmail.com>
2013-08-05 18:21:26 -0500

    update replies controller

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,8 +1,8 @@
 class Parent < ActiveRecord::Base
   attr_accessible :child_nm, :class_code, :delivery_time, :first_nm, :last_nm, :phone_number, :relationship, :state
   state_machine initial: :course_num_and_phone do
- 		event :added_course_number_and_phone do
-	 		transition :course_num_and_phone=> :first_nm
+ 		event :added_course_number_and_phone_number do
+	 		transition :course_num_and_phone => :first_nm
 	 	end
 
  		event :added_first_name do
