86346f1c28580f373ee481be3594decca7574867
Patricia Silva <pscorreo@gmail.com>
2013-08-08 16:30:04 -0500

    update pdf generator

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,7 @@
 class CoursesController < ApplicationController
   # GET /courses
   # GET /courses.json
+  require 'prawn'
   include CoursesHelper
   def index
     @courses = Course.all
@@ -90,20 +91,67 @@ class CoursesController < ApplicationController
 
   def generate_pdf
     @course =Course.find(params[:id])
+    course_id = params[:id]
     pdf = Prawn::Document.new
-    pdf.stroke_horizontal_rule
-    pdf.text "Receive Free Daily Classroom Updates!", :align => :center, :size => 22
-    pdf.bounding_box([0, 675], :width => 200, :height => 100) do
-      pdf.text "bringup", :align => :center, :size => 18, :font_color => "FF9200"
+    #pdf.stroke_bounds
+    image_url = 
+    
+    #colors for text
+    orange = "FF9200"
+    teal   = "009092"
+    black  = "000000"
+
+    pdf.text "Receive Free Daily Classroom Updates!", :align => :left, :size => 28
+    
+    pdf.bounding_box([0, 675], :width => 225, :height => 100) do
+      #pdf.stroke_bounds
+      pdf.fill_color "FF9200"
+      pdf.text "bringup", :align => :center, :size => 20
         end
-    pdf.bounding_box([250, 675], :width => 200, :height => 100) do
-      pdf.text "Sign Up for FREE!", :align => :center, :size => 18, :font_color => "FF9200"
-        end   
-    pdf.bounding_box([20, 0], :width => 200, :height => 400) do
-      pdf.text "Studies show that just asking your child how their school day was and showing genuine interest in the learning they are doing can have the same impact as hours of private tutoring.", :align => :left, :size => 11, :font_color => "FF9200"
+
+    pdf.bounding_box([325, 675], :width => 200, :height => 30) do
+      pdf.fill_color black
+      pdf.text "Sign Up for FREE!", :align => :center, :size => 20
+    end
+
+    pdf.image "#{Rails.root}/app/assets/images/Cell-Sketch-Green.png", :position => :right
+      
+    pdf.bounding_box([360, 550], :width =>200, :height =>30) do
+      pdf.text "Text:", :align =>:left, :size => 18
+    end
+
+     pdf.bounding_box([360, 530], :width =>200, :height =>30) do
+      pdf.fill_color "FF9200"
+      pdf.text course_id, :align =>:left, :size => 18
+    end
+
+    pdf.bounding_box([360, 500], :width =>200, :height =>30) do
+      pdf.fill_color black
+      pdf.text "To:", :align =>:left, :size => 18
+    end
+
+    pdf.bounding_box([360, 480], :width =>200, :height =>30) do
+      pdf.fill_color "FF9200"
+      pdf.text "+15124444444", :align =>:left, :size => 18
+    end
+
+
+    pdf.bounding_box([0, 645], :width => 225, :height => 400) do
+      #pdf.stroke_bounds
+      pdf.fill_color teal
+      pdf.text "Studies show that just asking your child how their school day was and showing genuine interest in the learning they are doing can have the same impact as hours of private tutoring!
+
+      Sign up to receive classroom recaps from your student's teacher every evening via SMS text message.
+
+      We will text you the necessary information to help you talk with your child about what they did in school every night.",
+       :align => :justify, :size => 11, :font_color => teal
         end
-    #pdf.move_down 260
-    #pdf.stroke_horizontal_rule   
+
+    pdf.bounding_box([0, 445], :width => 225, :height => 200) do
+      pdf.fill_color orange
+      pdf.text "See www.bringuptogether.com for more information", :align => :left
+      end    
+ 
     pdf_file_name = File.join(Rails.root, "public/pdfs", "#{@course.name}.pdf")
 
     pdf.render_file pdf_file_name
