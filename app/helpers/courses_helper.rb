e63074a5095cd2f052510a3a77baa241936de1ec
Patricia Silva <pscorreo@gmail.com>
2013-08-08 09:15:52 -0500

    refactor pdf generation

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,15 @@
 module CoursesHelper
+
+ def stroke_axis(options={})
+       options = { :height => bounds.height.to_i,
+                   :width => bounds.width.to_i
+                 }.merge(options)
+      
+       dash(1, :space => 4)
+       stroke_horizontal_line(-21, options[:width], :at => 0)
+       stroke_vertical_line(-21, options[:height], :at => 0)
+  end   
+
+ 
+
 end
