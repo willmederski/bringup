3bbc523a3757a1c1dd3f35578b3478270b1caf0f
Matt Buck <github@matthewbuck.com>
2013-08-08 14:46:34 -0500

    Start working on a Teacher spec.

--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -1,7 +1,17 @@
 require 'spec_helper'
 
-  # test "the truth" do
-  #   assert true
-  # end
+describe Teacher
+  context 'some context' do
+    let(:teacher) do
+      Teacher.new email:                 'teacher@school.com',
+                  full_name:             'Tom Teacher',
+                  phone:                 '555-1212',
+                  preferred_name:        'Tom',
+                  principal_name:        'Principal Person',
+                  school:                'MakerSquare',
+                  password:              'password',
+                  password_confirmation: 'password'
+    end
+  end
 end
 
