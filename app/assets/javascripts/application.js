60fdc4d23948702b722322c312f0ecb38e85bccf
Matt Buck <github@matthewbuck.com>
2013-08-08 15:24:51 -0500

    Refactor the counter code into a jQuery function.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,114 +25,35 @@
 
 $(document).ready(function()  {
 
-    var characters = 120;
-    var remaining = characters -  $("#body1").val().length;
-    $(".counter1").append(remaining+" left");
-    $("#body1").keyup(function(){
+    $.fn.counterField = function() {
+      var characters = 120;
+      var $counter = $(this).siblings('span');
+      var remaining = characters -  $(this).val().length;
+      $counter.append(remaining+" left");
+      $(this).keyup(function(){
         if($(this).val().length > characters){
-        $(this).val($(this).val().substr(0,characters));
+          $(this).val($(this).val().substr(0,characters));
         }
-    var remaining = characters -  $(this).val().length;
-    $(".counter1").html(remaining+" left");
-    if(remaining <= 20)
-    {
-        $(".counter1").css("color","red");
-    }
-    else
-    {
-        $(".counter1").css("color","black");
-    }
-    });
-
-
-
-    var characters = 120;
-    var remaining = characters -  $("#body2").val().length;
-    $(".counter2").append(remaining+" left");
-    $("#body2").keyup(function(){
-        if($(this).val().length > characters){
-        $(this).val($(this).val().substr(0,characters));
-        }
-    var remaining = characters -  $(this).val().length;
-    $(".counter2").html(remaining+" left");
-    if(remaining <= 20)
-    {
-        $(".counter2").css("color","red");
-    }
-    else
-    {
-        $(".counter2").css("color","black");
-    }
-    });
-
-
-
-    var characters = 120;
-    var remaining = characters -  $("#body3").val().length;
-    $(".counter3").append(remaining+" left");
-    $("#body3").keyup(function(){
-        if($(this).val().length > characters){
-        $(this).val($(this).val().substr(0,characters));
-        }
-    var remaining = characters -  $(this).val().length;
-    $(".counter3").html(remaining+" left");
-    if(remaining <= 20)
-    {
-        $(".counter3").css("color","red");
-    }
-    else
-    {
-        $(".counter3").css("color","black");
-    }
-    });
-
-
-
-    var characters = 120;
-    var remaining = characters -  $("#body4").val().length;
-    $(".counter4").append(remaining+" left");
-    $("#body4").keyup(function(){
-        if($(this).val().length > characters){
-        $(this).val($(this).val().substr(0,characters));
-        }
-    var remaining = characters -  $(this).val().length;
-    $(".counter4").html(remaining+" left");
-    if(remaining <= 20)
-    {
-        $(".counter4").css("color","red");
-    }
-    else
-    {
-        $(".counter4").css("color","black");
-    }
-    });
-
-
-
-    var characters = 120;
-    var remaining = characters -  $("#body5").val().length;
-    $(".counter5").append(remaining+" left");
-    $("#body5").keyup(function(){
-        if($(this).val().length > characters){
-        $(this).val($(this).val().substr(0,characters));
-        }
-    var remaining = characters -  $(this).val().length;
-    $(".counter5").html(remaining+" left");
+        var remaining = characters -  $(this).val().length;
+        $counter.html(remaining+" left");
         if(remaining <= 20)
         {
-            $(".counter5").css("color","red");
+          $counter.css("color","red");
         }
         else
         {
-            $(".counter5").css("color","black");
+          $counter.css("color","black");
         }
-    });
+      });
+    };
 
+    $('#body1').counterField();
+    $('#body2').counterField();
+    $('#body3').counterField();
+    $('#body4').counterField();
+    $('#body5').counterField();
 
     $(function() {
       $('.datepicker').datepicker();
     });
-
-
- 
 });
