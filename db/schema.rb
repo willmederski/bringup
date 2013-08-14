1403ec90f3e777504f1107e77b058d08fa4dbe50
Lydia Guarino <lydiahguarino@gmail.com>
2013-08-14 18:45:43 -0500

    fixed some responsive details for views

--- db/schema.rb
+++ db/schema.rb
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(:version => 20130814164038) do
     t.string   "uri"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+    t.string   "state"
   end
 
   create_table "schools", :force => true do |t|
